87540178d1cd3d0a5aa1d39f994295cf46e8e792
Dasari Srividya <srividyadasari1610@gmail.com>
2026-07-05 19:32:48 +0530

    Add files via upload

---
+++ java_fundamentals/flow_control_statements/HandsOnAssignment1A.java
@@ -0,0 +1,12 @@
+class HandsOnAssignment1A {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+
+        if (n > 0)
+            System.out.println("Positive");
+        else if (n < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+    }
+}
