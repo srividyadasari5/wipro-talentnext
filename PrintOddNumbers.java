7940be2fa645002d13046be78912e4f4f0e7c067
srividyadasari5 <srividyadasari1610@gmail.com>
2026-07-09 11:14:20 +0530

    Added Day 2 logic buliding Java programs

---
+++ PrintOddNumbers.java
@@ -0,0 +1,9 @@
+public class PrintOddNumbers {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 99; i += 2) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
