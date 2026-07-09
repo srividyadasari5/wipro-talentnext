9c0d81b5428a7e6dbb746a025c8e6a7c4c4212b5
Dasari Srividya <srividyadasari1610@gmail.com>
2026-07-09 11:25:34 +0530

    Delete PrintOddNumbers.java

--- PrintOddNumbers.java
+++
@@ -1,9 +0,0 @@
-public class PrintOddNumbers {
-    public static void main(String[] args) {
-
-        for (int i = 1; i <= 99; i += 2) {
-            System.out.print(i + " ");
-        }
-
-    }
-}
