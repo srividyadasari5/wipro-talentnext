0146590519efd2cd9af515e5586459dfac9e3346
Dasari Srividya <srividyadasari1610@gmail.com>
2026-07-23 09:23:45 +0530

    Completed ThreadControl_and_Priorites Assignments

---
+++ MultiThreading/ThreadControl_and_Priorites/HandsOnAssignment1.java
@@ -0,0 +1,28 @@
+class NumberThread extends Thread {
+
+    @Override
+    public void run() {
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(i);
+
+            if (i == 5) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+}
+
+public class HandsOnAssignment1 {
+
+    public static void main(String[] args) {
+
+        NumberThread t = new NumberThread();
+        t.start();
+    }
+}
---
+++ MultiThreading/ThreadControl_and_Priorites/HandsOnAssignment3.java
@@ -0,0 +1,34 @@
+class PriorityThread extends Thread {
+
+    public PriorityThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(getName() + " : " + i);
+        }
+
+        System.out.println(getName() + " completed.");
+    }
+}
+
+public class HandsOnAssignment3 {
+
+    public static void main(String[] args) {
+
+        PriorityThread t1 = new PriorityThread("MAX Priority Thread");
+        PriorityThread t2 = new PriorityThread("MIN Priority Thread");
+        PriorityThread t3 = new PriorityThread("NORM Priority Thread");
+
+        t1.setPriority(Thread.MAX_PRIORITY);
+        t2.setPriority(Thread.MIN_PRIORITY);
+        t3.setPriority(Thread.NORM_PRIORITY);
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
